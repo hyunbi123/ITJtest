397cdc70e89eeb19da9b72d0c72ad8d9441dca06
hyunbi123 <junghyunbi512@naver.com>
2025-03-17 04:40:52 +0900

    2

---
+++ src/main/java/com/example/itjtest/ItJtestApplication.java
@@ -0,0 +1,13 @@
+package com.example.itjtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItJtestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItJtestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/itjtest/ItJtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.itjtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItJtestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
